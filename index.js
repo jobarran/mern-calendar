182c4285c1bbffb98c1e2bdc438b12405d995c90
Joaquin Barrandeguy <joaquinbarrandeguy@gmail.com>
2023-04-02 09:56:15 -0300

    Path * agregado 2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use( express.json() )
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
-app.get('#', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile( __dirname + '/public/index.html');
 })
 
